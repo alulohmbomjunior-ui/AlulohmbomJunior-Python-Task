e81e4e795043969acc48dbd54c5a550483d200e8
alulohmbomjunior-ui <alulohmbomjunior@gmail.com>
2026-04-11 07:57:04 +0100

    Add files via upload

---
+++ Alulohmbom Junior C++ assignment on Prime numbers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 == 0 || num % 3 == 0) return false;
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << "Prime numbers from 1 to " << n << " are: ";
+    for (int i = 2; i <= n; ++i) {
+        if (isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
